cbe1c71edf9d7cc2be030784084d3a1133fae88d
Ksenia <73356609+Kse-Nia@users.noreply.github.com>
2026-04-15 10:13:53 +0200

    feat: update and add route for UserProfile page

--- frontend-argentbank/src/App.tsx
+++ frontend-argentbank/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
+import UserProfile from "./pages/UserProfile";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/profile" element={<UserProfile />} />
         </Route>
       </Routes>
     </>
