f08b286ecbfc5e45dfe317553e873a48e83a07a4
Ksenia <73356609+Kse-Nia@users.noreply.github.com>
2026-04-15 09:14:29 +0200

    chore: update with right state

--- frontend-argentbank/src/types/types.ts
+++ frontend-argentbank/src/types/types.ts
@@ -8,13 +8,3 @@ export interface UserState {
   isLoading: boolean;
   error: string | null;
 }
-
-// Define a type for the slice state
-export interface CounterState {
-  value: number
-}
-
-// Define the initial state using that type
-const initialState: CounterState = {
-  value: 0,
-}
