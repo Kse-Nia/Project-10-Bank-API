82c012ee12c0f3e4267e133cfbdfc78198904ae9
Ksenia <73356609+Kse-Nia@users.noreply.github.com>
2026-06-03 11:19:06 +0200

    feat: update comments doc

--- frontend-argentbank/src/config/api.ts
+++ frontend-argentbank/src/config/api.ts
@@ -1,9 +1,8 @@
 // API Configuration
 export const API_BASE_URL = import.meta.env.VITE_API_URL; // Import from .env file
 
-
 export const API_ENDPOINTS = {
-  LOGIN: `${API_BASE_URL}/user/login`,
-  SIGNUP: `${API_BASE_URL}/user/signup`,
-  USER_PROFILE: `${API_BASE_URL}/user/profile`,
+  LOGIN: `${API_BASE_URL}/user/login`, // User Login
+  SIGNUP: `${API_BASE_URL}/user/signup`, // Register New User
+  USER_PROFILE: `${API_BASE_URL}/user/profile`, // Fetch and Update User Profile
 };
